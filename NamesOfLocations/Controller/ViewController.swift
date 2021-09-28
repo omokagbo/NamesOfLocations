8e9822b93294dc84b942b41042d74fbddb2b1a84
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-09-28 09:45:44 +0100

    add pin to a location on the map

--- NamesOfLocations/Controller/ViewController.swift
+++ NamesOfLocations/Controller/ViewController.swift
@@ -12,17 +12,33 @@ class ViewController: UIViewController {
     
     private let mapView: MKMapView = {
         let map = MKMapView()
+        map.isScrollEnabled = true
+        map.isZoomEnabled = true
+        map.showsUserLocation = true
         return map
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(mapView)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        addUserLocationToMap()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         mapView.frame = view.bounds
+    }
+    
+    private func addUserLocationToMap() {
+        LocationManager.shared.getUserLocation { [weak self] location in
+            DispatchQueue.main.async {
+                let pin = MKPointAnnotation()
+                pin.coordinate = location.coordinate
+                self?.mapView.setRegion(MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000), animated: true)
+                self?.mapView.addAnnotation(pin)
+            }
+        }
     }
     
 }
